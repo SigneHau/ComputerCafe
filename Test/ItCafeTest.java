a7d91c32b32bf7f69301f251b4690f7d51a21530
Signe Kjær Brandt Hau <signehau@gmail.com>
2023-08-24 11:35:30 +0200

    ItCafe og Test
    Itcafe og test klssen er lavet

---
+++ Test/ItCafeTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ItCafeTest {
+
+    @Test
+    public void testComputerBookingOptionOutOfRange() {
+        ItCafe itCafe = new ItCafe();
+
+
+        String input = "5\n"; // Simulate user input: 4 and then Enter
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThrows(ArithmeticException.class, itCafe::ComputerBooking);
+
+
+    }
+
+}
+
