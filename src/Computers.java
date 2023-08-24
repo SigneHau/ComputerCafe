81d9df74148d7c4ecab6f9141c6ddb23d977af03
Ingridksv <ingrid.s.96@hotmail.com>
2023-08-24 11:03:49 +0200

    Computers
    Computers klasse, konstruktør og første metode

--- src/Computers.java
+++ src/Computers.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Computers {
     private boolean[] availability;
 
@@ -6,5 +8,18 @@ public class Computers {
         for (int i = 0; i < availability.length; i++){
             availability[i] = true; //All computers are initially available
         }
+    }
+
+    //Metode som returnerer en arrayliste af typen Integer, som viser hvilke der er frie.
+    public ArrayList<Integer> getAvailable(){
+        ArrayList<Integer>list = new ArrayList<>();
+
+//Løber gennem forloop, for at se om den er ledig eller ej.
+        for (int i = 0;i<availability.length;i++){
+            if(availability[i]){
+                list.add(i);
+            }
+        }
+        return list;
     }
 }
