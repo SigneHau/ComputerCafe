018e7c368d232c791f6f0c8aa939ee265feec863
Signe Kjær Brandt Hau <signehau@gmail.com>
2023-08-24 10:46:44 +0200

    ItCafe
    Opstart

---
+++ src/ItCafe.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ItCafe {
+
+//Hoved menuen - som jeg vil teste på.
+    public void ComputerBooking() {
+        System.out.println("Menu: Choose an option: ");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("1-Check available computers ");
+        System.out.println("2-Do you want to book a computer?");
+        System.out.println("3-Are you done?");
+    }
+
+
+}
